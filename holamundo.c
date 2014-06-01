7b6b3a1c7f4f873fa5515fba372e1860a4a22730
i02mamom <i02mamom@uco.es>
2014-06-01 23:54:21 +0200

    Change i02mamom

--- holamundo.c
+++ holamundo.c
@@ -4,7 +4,8 @@ int main(int argc, char *argv[])
 {
   char c[100] = "Hola mundo";
 
-  printf("%s\n", strcat(c, ", soy i52gobas") );
+  printf("%s\n", strcat(c, ", soy i52gobas") );	
+  printf("%s\n", strcat(c, ", clonado por: i02mamom") );
   return 0;
 }
 
